1bd0858ebe66395eb62beaa5c8acfa2eee83bd28
YangYuesong <E0015005@u.nus.edu>
2019-03-18 21:17:37 +0800

    Timeline

---
+++ img/horizontal-timeline/js/index.js
@@ -0,0 +1,130 @@
+// Globals
+var prefixes         = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+var $container       = $('.container');
+var $timeline        = $('.timeline');
+var $timelineItem    = $('.timeline-item');
+var $timelineContent = $('.timeline-content');
+var $dropDown        = $('.dropdown');
+var $hasHovered      = true;
+var hideOnExit       = false;
+
+// mouseenter event handler
+$timelineItem.on('mouseenter', function(e) {
+  
+  var isSelected = $(this).hasClass('selected');
+  
+  if ( isSelected === false ) {
+  
+    var leftPos = $(this).position().left,
+        left    = leftPos - 88,
+        $that   = $(this);
+
+    $timelineItem.removeClass('selected');
+    $(this).addClass('selected');
+
+    if ( $hasHovered === false ) {
+      // Show Bounce
+
+        // Set Flag
+        $hasHovered = true;
+
+        // Show DD Bounce
+        showBounce(left);
+
+        // Show DD content Bounce
+        showContentBounce($that);
+
+    } else {
+      // Follow
+
+        // Change pos of DD to follow
+        dropDownFollow(left);
+
+        // Hide previous dd content
+        $timelineContent.removeClass('animated fadeIn bounceIn');
+
+        // Show hovered dd content
+        $that.find($timelineContent).addClass('animated fadeIn');
+    }
+  }
+  
+});
+
+// mouseleave event handler
+$timeline.on('mouseleave', function(e) {
+  
+  if (hideOnExit) {
+   
+    //   Set Flag
+    $hasHovered = false;
+
+    // Hide DD
+    hideDropDown();
+
+    // Hide DD content
+    $timelineContent.removeClass('animated fadeIn');
+    
+  }
+  
+});
+
+// Animation end event listener
+$dropDown.on(prefixes, function(e) {
+  
+  if ( e.originalEvent.animationName === 'fadeOut' ) {
+    $dropDown.removeAttr('style');
+  }
+  
+});
+
+/**
+* Private functions that do showing/hiding/animating
+*/
+function showContentBounce(that) {
+  $hasBounced = true;
+  that.find('.timeline-content').addClass('animated bounceIn');
+}
+
+function showBounce(pos) {
+  $dropDown.css('left', pos + 'px').removeClass('fadeOut').addClass('animated bounceIn');
+}
+
+function dropDownFollow(pos) {
+  $dropDown.css('left', pos + 'px');
+}
+
+function hideDropDown() {
+  $timelineItem.removeClass('selected');
+  $dropDown.removeClass('bounceIn').addClass('fadeOut');
+}
+
+var TimeLine = new Vue({
+    el: '#TimeLine',
+    data: {
+        user: 'Bolin',
+        tasks: [
+            {
+                name: "draft submission",
+                date: "1/2/2018",
+                status : "completed"
+            },
+            {
+                name: "final submission",
+                date: '2/4/2018',
+                status : 'completed'
+            }
+        ]
+    },
+    methods:{
+        add: function(){
+            console.log("Adding task");
+            
+            var newtask = {
+                name:"555",
+                date:"666",
+                status: "444"
+            };
+            this.tasks.push(newtask);
+        }
+    }
+})
